bd8932f1492665c88273dff1f36d4ee03254eb75
Julian <julian@wheresnick.com.au>
2024-01-01 19:31:47 +1100

    added and fixed up server file

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
-const { userRoutes, thoughtRoutes } = require('./routes/api');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
+
+const { userRoutes, thoughtRoutes } = require('./routes/api');
 
 app.use(express.json());
+
+// Mongoose
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+  });
 
 // Using routes
 app.use('/api', userRoutes);
 app.use('/api', thoughtRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
